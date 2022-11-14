b09e384e097480839c56f756dc2e1b8b3aa3fbdb
andrew <asc269@cornell.edu>
2022-11-14 14:04:19 -0500

    Update project - Andrew Choi (asc269)

---
+++ scripts/icon-darken.js
@@ -0,0 +1,23 @@
+$("#home").hover(() => {
+  $("#home").addClass("hover-bg")
+}, () => {
+  $("#home").removeClass("hover-bg")
+})
+
+$("#past-events").hover(() => {
+  $("#past-events").addClass("hover-bg")
+}, () => {
+  $("#past-events").removeClass("hover-bg")
+})
+
+$("#apply").hover(() => {
+  $("#apply").addClass("hover-bg")
+}, () => {
+  $("#apply").removeClass("hover-bg")
+})
+
+$("#faq").hover(() => {
+  $("#faq").addClass("hover-bg")
+}, () => {
+  $("#faq").removeClass("hover-bg")
+})
