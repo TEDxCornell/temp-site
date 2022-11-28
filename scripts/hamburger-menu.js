f12e172754db669ff7ca85e0c89c7d68a865bce0
Flavia Jiang <yj472@cornell.edu>
2022-11-27 22:13:16 -0500

    implemented hamburger menu for narrow screens

--- scripts/hamburger-menu.js
+++ scripts/hamburger-menu.js
@@ -1,1 +1,13 @@
-/* We are will working on it.**/
+$("#navbutton").click(function() {
+    if ($("#cross").hasClass("hidden")) {
+        $("#cross").removeClass("hidden");
+        $("#burger").addClass("hidden");
+        $("#main-nav").removeClass("hidden");
+        $(".main").addClass("transparent");
+      } else {
+        $("#cross").addClass("hidden");
+        $("#burger").removeClass("hidden");
+        $("#main-nav").addClass("hidden");
+        $(".main").removeClass("transparent");
+      }
+  });
