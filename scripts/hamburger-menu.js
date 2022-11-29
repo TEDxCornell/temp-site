10226ba3ee7e2da71770fea6d8e0c262fabf4669
Flavia Jiang <yj472@cornell.edu>
2022-11-29 17:30:04 -0500

    made code more organized

--- scripts/hamburger-menu.js
+++ scripts/hamburger-menu.js
@@ -11,3 +11,28 @@ $("#navbutton").click(function() {
         $(".main").removeClass("transparent");
       }
   });
+
+  $(".events-button").click(function() {
+    if ($(".events-button > .arrow-up").hasClass("hidden")) {
+        $(".events-button > .arrow-up").removeClass("hidden");
+        $(".events-button > .arrow-down").addClass("hidden");
+        $(".dropdown").removeClass("hidden");
+      } else {
+        $(".events-button > .arrow-up").addClass("hidden");
+        $(".events-button > .arrow-down").removeClass("hidden");
+        $(".dropdown").addClass("hidden");
+      }
+    if ($(".dropdown").hasClass("hidden_dropdown")){
+      $(".dropdown").removeClass("hidden_dropdown");
+    } else{
+      $(".dropdown").addClass("hidden_dropdown");
+    }
+  });
+
+  $(".subsection").click(function() {
+    $("#cross").addClass("hidden");
+    $("#burger").removeClass("hidden");
+    $("#main-nav").addClass("hidden");
+    $(".main").removeClass("transparent");
+    }
+);
